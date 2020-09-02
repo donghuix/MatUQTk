feb73fc5d09d0362fc6b6fc9a6122310032c3697
donghuix <donghui.xu@pnnl.gov>
2020-09-02 09:05:04 -0700

    Create m_pce_bcs.m

---
+++ pce_bcs/m_pce_bcs.m
@@ -0,0 +1,102 @@
+function [ytrain_pc, yval_pc, pccf_all, mindex_all] = m_pce_bcs(uqtkbin,pars,xtrain,ytrain,xval,yval,del_opt)
+    
+    if nargins < 7
+        del_opt = 1; % Default mode is to delete all the files
+    end
+    
+    [ntrain,nout] = size(ytrain);
+    [nval,~]      = size(yval);
+    assert(ntrain == pars.ntrain & nval == pars.nval);
+    
+    % Pre-allocate arrays and lists to store results
+    pccf_all  = cell(nout,1);
+    mindex_all= cell(nout,1);
+    ytrain_pc = NaN(ntrain,nout);
+    yval_pc   = NaN(nval,nout);
+    err_train = NaN(nout,1);
+    err_val   = NaN(nout,1);
+    
+    % Read parameters
+    pc_type   = pars.pc_type;
+    in_pcdim  = pars.in_pcdim;
+    out_pcord = pars.out_pcord;
+    pred_mode = pars.pred_mode;
+    tol       = pars.tol;
+    
+    fprintf('\n\n************ Trainning Surrogate Model ************\n');
+    for i = 1 : nout
+        
+        fprintf('\n###################################################\n');
+        fprintf(['-------------------- ' num2str(i) 'th QOI --------------------\n\n']);
+        ydata = ytrain(:,i);
+        
+        %save('ydata.dat','ydata','-ascii');
+        dlmwrite('ydata.dat',ydata,' ');
+        % Generate PC multiindex
+        if ispc
+            cmd= [uqtkbin 'gen_mi.exe -x"TO" -p' num2str(out_pcord) ' -q' num2str(in_pcdim) ' > gmi.log'];
+            fprintf(['Running gen_mi.exe -x"TO" -p' num2str(out_pcord) ' -q' num2str(in_pcdim) ' > gmi.log \n']);
+        else
+            cmd= [uqtkbin 'gen_mi -x"TO" -p' num2str(out_pcord) ' -q' num2str(in_pcdim) ' > gmi.log; mv mindex.dat mi.dat'];
+            fprintf(['Running ' cmd '\n']);
+        end
+   
+        [status,cmdout] = system(cmd,'-echo');
+        if ispc
+            system('mv mindex.dat mi.dat');
+        end
+        mi=load('mi.dat'); 
+        [npc,~] = size(mi);
+
+        xcheck    = [xtrain; xval];
+        regparams = ones(npc,1);
+        save('xdata.dat','xtrain','-ascii');
+        save('xcheck.dat','xcheck','-ascii');
+        save('regparams.dat','regparams','-ascii');
+        
+        if ispc
+            cmd=[uqtkbin 'regression.exe -x xdata.dat -y ydata.dat -b PC_MI -s ' pc_type ' -p mi.dat -w regparams.dat -m ' pred_mode ' -r wbcs -t xcheck.dat -c ' num2str(tol) ' > regr.log'];
+            fprintf(['Running regression.exe -x xdata.dat -y ydata.dat -b PC_MI -s ' pc_type ' -p mi.dat -w regparams.dat -m ' pred_mode ' -r wbcs -t xcheck.dat -c ' num2str(tol) ' > regr.log \n']);
+        else
+            cmd=[uqtkbin 'regression -x xdata.dat -y ydata.dat -b PC_MI -s ' pc_type ' -p mi.dat -w regparams.dat -m ' pred_mode ' -r wbcs -t xcheck.dat -c ' num2str(tol) ' > regr.log'];
+            fprintf(['Running ' cmd '\n']);
+        end
+        
+        [status,cmdout] = system(cmd,'-echo');
+        
+        fprintf(cmdout);
+        
+        % Get the PC coefficients and multiindex and the predictive errorbars
+        pccf   = load('coeff.dat');
+        mindex = load('mindex_new.dat'); 
+
+        % Append the results
+        pccf_all{i}   = pccf;
+        mindex_all{i} = mindex;
+
+        % Evaluate surrogate at training points
+        sprintf('Evaluating surrogate at %d training points', ntrain);
+        ytrain_pc(:,i) = model_pc(uqtkbin,xtrain,mindex,pccf,pc_type,del_opt);
+        err_train(i)=norm(ytrain(:,i)-ytrain_pc(:,i))/norm(ytrain(:,i));
+        fprintf(['Surrogate relative error at training points : ' num2str(err_train(i)) '\n']);
+
+        % Evaluate surrogate at validate points
+        sprintf('Evaluating surrogate at %d training points', nval);
+        yval_pc(:,i) = model_pc(uqtkbin,xval,mindex,pccf,pc_type,del_opt);
+        err_val(i) = norm(yval(:,i)-yval_pc(:,i))/norm(yval(:,i));
+        fprintf(['Surrogate relative error at validation points : ' num2str(err_val(i)) '\n']);
+        fprintf('\n###################################################\n');
+        
+    end
+    % delte files
+    if del_opt
+        delete xcheck.dat; delete regparams.dat; delete regr.log;
+        delete mi.dat; delete gmi.log; delete mindex_new.dat; delete coeff.dat;
+
+        delete lambdas.dat; delete selected.dat; delete Sig.dat; delete sigma2.dat;
+        delete ycheck.dat; delete ycheck_var.dat;
+    end
+end
+    
+
+
