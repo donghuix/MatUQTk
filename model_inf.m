e295d184346d974c0410120efd0b35c59923ce2f
donghuix <donghui.xu@pnnl.gov>
2021-04-23 13:35:25 -0700

    Update model_inf.m

--- model_inf.m
+++ model_inf.m
@@ -1,4 +1,4 @@
-function mapparam = model_inf(uqtkbin, X, Y, pars, mindex_all, pccf_all, del_opt)
+function [mapparam,pchain] = model_inf(uqtkbin, X, Y, pars, mindex_all, pccf_all, del_opt)
 % INPUT:
 % X [ N, S ], N values of inputs, S is dimension of xi
 % Y [ N, 1 ], N values of observations
@@ -24,9 +24,12 @@ function mapparam = model_inf(uqtkbin, X, Y, pars, mindex_all, pccf_all, del_opt
 
         mindex = mindex_all{i};
         pccf   = pccf_all{i};
-
+        
         dlmwrite(['mindexp.' num2str(i-1) '.dat'], mindex, ' ');
         dlmwrite(['pccfp.' num2str(i-1) '.dat'], pccf, ' ');
+        
+        dlmwrite(['mindexp.' num2str(i-1) '_pred.dat'], mindex, ' ');
+        dlmwrite(['pccfp.' num2str(i-1) '_pred.dat'], pccf, ' ');
 
     end
     
@@ -41,23 +44,24 @@ function mapparam = model_inf(uqtkbin, X, Y, pars, mindex_all, pccf_all, del_opt
         cmd =[uqtkbin 'model_inf.exe -f pcs -l classical -a' num2str(a) ' -b ' num2str(b) ' -d ' num2str(PDIM) ' -m 10000 -o ' num2str(pars.out_pcord) ' > inference.log'];
         fprintf(['Running ' cmd]);
     else
-        cmd = [uqtkbin 'model_inf -f pcs -l classical -a' num2str(a) ' -b ' num2str(b) ' -d ' num2str(PDIM) ' -m 10000 -o ' num2str(pars.out_pcord) ' > inference.log'];
+        cmd = [uqtkbin 'model_inf -f pcs -l classical -a' num2str(a) ' -b ' num2str(b) ' -g 1 -d ' num2str(PDIM) ' -m 10000 -o ' num2str(pars.out_pcord) ' > inference.log'];
         fprintf(['Running ' cmd '\n']);
     end
     [status,cmdout] = system(cmd,'-echo');
-    chain = load('chain.dat');
-    likelihood = chain(:,end);
-    ibest = find(likelihood == max(likelihood));
-    mapparam = nanmean(chain(ibest,2:end-3),1);
-    %mapparam = load('mapparam.dat');
+    %chain = load('chain.dat');
+    %likelihood = chain(:,end);
+    %ibest = find(likelihood == max(likelihood));
+    %mapparam = nanmean(chain(ibest,2:end-3),1);
+    mapparam = load('mapparam.dat');
+    pchain   = load('pchain.dat');
     
     if del_opt
         delete mindexp*dat pccfp*dat;
-        %delete xdata.dat ydata.dat;
-%         delete parampccfs.dat fmeans_sams.dat datavars.dat fvars.dat fmeans.dat ...
-%                pvars.dat pmeans.dat mapparam.dat pchain.dat;
-        % Do NOT delete chain.dat to be able to construct posterior PDF
-        % delete chain.dat;
+        delete xdata.dat ydata.dat;
+        delete parampccfs.dat fmeans_sams.dat datavars.dat fvars.dat fmeans.dat ...
+               pvars.dat pmeans.dat mapparam.dat pchain.dat;
+        %Do NOT delete chain.dat to be able to construct posterior PDF
+        delete chain.dat;
     end
 
 end
