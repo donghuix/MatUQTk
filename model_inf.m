8ce37851e9aac6242b3d80152dc932d035a3659e
donghuix <donghui.xu@pnnl.gov>
2021-04-23 15:09:32 -0700

    Update model_inf.m

--- model_inf.m
+++ model_inf.m
@@ -1,7 +1,21 @@
-function [mapparam,pchain] = model_inf(uqtkbin, X, Y, pars, mindex_all, pccf_all, del_opt)
+function [mapparam,pchain] = model_inf(uqtkbin, X, Y, pars, mindex_all, pccf_all, del_opt, currdir, tag)
 % INPUT:
-% X [ N, S ], N values of inputs, S is dimension of xi
-% Y [ N, 1 ], N values of observations
+% uqtkbin: UQTk installed function dirctory
+% X [ N ]: design or controllable parameter
+% Y [ N ]: observations
+% pars   : MatUQTk parameter structure
+% mindex_all
+% pccf_all
+% del_opt: del_opt = 1 -> delete all the middle files 
+% currdir, tag: for parallel processing 
+
+    if nargin == 7
+        parallel_mode = 0;
+    elseif nargin == 9
+        parallel_mode = 1;
+    else
+        error('Check the input!!!');
+    end
     if isempty(X)
         X = 1 : size(Y,1);
         X = X';
@@ -14,11 +28,18 @@ function [mapparam,pchain] = model_inf(uqtkbin, X, Y, pars, mindex_all, pccf_all
 
     assert(nx == ny, 'Check the size of X and Y!!!\n');
     assert(nm == np, 'Cehck the size of PC data!!!\n');
-
+    
+    if parallel_mode
+        workdir = fullfile(currdir,['tmp' tag]);
+        if ~exist(workdir,'dir')
+            mkdir(workdir);
+        end
+        cd(workdir);
+    end
     dlmwrite('xdata.dat', X, ' ');
     dlmwrite('ydata.dat', Y, ' ');
-    dlmwrite('xdata1.dat', X, ' ');
-    dlmwrite('ydata1.dat', Y, ' ');
+%     dlmwrite('xdata1.dat', X, ' ');
+%     dlmwrite('ydata1.dat', Y, ' ');
 
     for i = 1 : nm
 
@@ -41,10 +62,10 @@ function [mapparam,pchain] = model_inf(uqtkbin, X, Y, pars, mindex_all, pccf_all
     
     %cmd = [uqtkbin 'model_inf -f pcs -l classical -d ' num2str(PDIM) ' > inference.log'];
     if ispc
-        cmd =[uqtkbin 'model_inf.exe -f pcs -l classical -a' num2str(a) ' -b ' num2str(b) ' -d ' num2str(PDIM) ' -m 10000 -o ' num2str(pars.out_pcord) ' > inference.log'];
+        cmd =[uqtkbin 'model_inf.exe -f pcs -l classical -a ' num2str(a) ' -b ' num2str(b) ' -d ' num2str(PDIM) ' -m 10000 -o ' num2str(pars.out_pcord) ' > inference.log'];
         fprintf(['Running ' cmd]);
     else
-        cmd = [uqtkbin 'model_inf -f pcs -l classical -a' num2str(a) ' -b ' num2str(b) ' -g 1 -d ' num2str(PDIM) ' -m 10000 -o ' num2str(pars.out_pcord) ' > inference.log'];
+        cmd = [uqtkbin 'model_inf -f pcs -l classical -a ' num2str(a) ' -b ' num2str(b) ' -d ' num2str(PDIM) ' -m 10000 -o ' num2str(pars.out_pcord) ' > inference.log'];
         fprintf(['Running ' cmd '\n']);
     end
     [status,cmdout] = system(cmd,'-echo');
@@ -54,14 +75,21 @@ function [mapparam,pchain] = model_inf(uqtkbin, X, Y, pars, mindex_all, pccf_all
     %mapparam = nanmean(chain(ibest,2:end-3),1);
     mapparam = load('mapparam.dat');
     pchain   = load('pchain.dat');
-    
+    if parallel_mode
+        cd(currdir);
+    end
     if del_opt
-        delete mindexp*dat pccfp*dat;
-        delete xdata.dat ydata.dat;
-        delete parampccfs.dat fmeans_sams.dat datavars.dat fvars.dat fmeans.dat ...
-               pvars.dat pmeans.dat mapparam.dat pchain.dat;
-        %Do NOT delete chain.dat to be able to construct posterior PDF
-        delete chain.dat;
+        if parallel_mode
+            delete(fullfile(['tmp' tag],'*'));
+            rmdir(fullfile(['tmp' tag]));
+        else
+            delete mindexp*dat pccfp*dat;
+            delete xdata.dat ydata.dat;
+            delete parampccfs.dat fmeans_sams.dat datavars.dat fvars.dat fmeans.dat ...
+                   pvars.dat pmeans.dat mapparam.dat pchain.dat;
+            %Do NOT delete chain.dat to be able to construct posterior PDF
+            delete chain.dat;
+        end
     end
 
 end
