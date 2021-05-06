e21344140c4a764d84eb250e1359a8c4597013fe
Donghui Xu <donghui.xu@pnnl.gov>
2021-05-05 22:17:47 -0700

    update pytools.py

--- pytools/pytools.py
+++ pytools/pytools.py
@@ -18,8 +18,12 @@ def p_pce_bcs(uqtkbin,pars,xtrain,ytrain,xval,yval,del_opt,cur_dir=None,tag=None
     mindex_all    = []
     ytrain_pc     = np.empty((ntrain,nout))
     yval_pc       = np.empty((nval,nout))
-    err_train     = np.empty((ntrain,nout))
-    err_val       = np.empty((nval,nout))
+    ytrain_pc[:]  = np.nan
+    yval_pc[:]    = np.nan
+    err_train     = np.empty((ntrain,1))
+    err_val       = np.empty((nval,1))
+    err_train[:]  = np.nan
+    err_val[:]    = np.nan
 
     pc_type       = pars['pc_type']
     in_pcdim      = pars['in_pcdim']
@@ -39,62 +43,66 @@ def p_pce_bcs(uqtkbin,pars,xtrain,ytrain,xval,yval,del_opt,cur_dir=None,tag=None
         print('-------------------- ' + str(i) + 'th QOI --------------------')
         ydata = ytrain[:,i]
 
-        np.savetxt('ydata.dat',ydata,delimiter='\t')
+        if np.isnan(ydata).all():
+            pccf_all.append(np.nan)
+            mindex_all.append(np.nan)
+        else:
+            np.savetxt('ydata.dat',ydata,delimiter='\t')
 
-        if platform == 'darwin' or platform == 'linux':
-            cmd = uqtkbin + 'gen_mi -x"TO" -p ' + str(out_pcord) + ' -q' + str(in_pcdim) + ' > gmi.log'
-        elif platform == 'win32':
-            cmd = uqtkbin + 'gen_mi.exe -x"TO" -p ' + str(out_pcord) + ' -q' + str(in_pcdim) + ' > gmi.log'
-        print('Running ' + cmd)
+            if platform == 'darwin' or platform == 'linux':
+                cmd = uqtkbin + 'gen_mi -x"TO" -p ' + str(out_pcord) + ' -q' + str(in_pcdim) + ' > gmi.log'
+            elif platform == 'win32':
+                cmd = uqtkbin + 'gen_mi.exe -x"TO" -p ' + str(out_pcord) + ' -q' + str(in_pcdim) + ' > gmi.log'
+            print('Running ' + cmd)
 
-        os.system(cmd)
+            os.system(cmd)
 
-        if platform == 'win32':
-            os.system('mv mindex.dat mi.dat')
-        elif platform == 'darwin' or platform == 'linux':
-            os.system('mv mindex.dat mi.dat')
-        mi = np.loadtxt('mi.dat')
-        npc = mi.shape[0]
+            if platform == 'win32':
+                os.system('mv mindex.dat mi.dat')
+            elif platform == 'darwin' or platform == 'linux':
+                os.system('mv mindex.dat mi.dat')
+            mi = np.loadtxt('mi.dat')
+            npc = mi.shape[0]
 
-        xcheck = np.vstack((xtrain,xval))
-        regparams = np.ones((npc,1))
+            xcheck = np.vstack((xtrain,xval))
+            regparams = np.ones((npc,1))
 
-        np.savetxt('xdata.dat',xtrain,delimiter='\t')
-        np.savetxt('xcheck.dat',xcheck,delimiter='\t')
-        np.savetxt('regparams.dat',regparams,delimiter='\t')
+            np.savetxt('xdata.dat',xtrain,delimiter='\t')
+            np.savetxt('xcheck.dat',xcheck,delimiter='\t')
+            np.savetxt('regparams.dat',regparams,delimiter='\t')
 
-        if platform == 'darwin' or platform == 'linux':
-            cmd = uqtkbin + 'regression -x xdata.dat -y ydata.dat -b PC_MI -s ' + pc_type +       \
-                    ' -p mi.dat -w regparams.dat -m ' + pred_mode + ' -r wbcs -t xcheck.dat -c ' + \
-                    str(tol) + ' > regr.log'
-        elif platform == 'win32':
-            cmd = uqtkbin + 'regression.exe -x xdata.dat -y ydata.dat -b PC_MI -s ' + pc_type +   \
-                    ' -p mi.dat -w regparams.dat -m ' + pred_mode + ' -r wbcs -t xcheck.dat -c ' + \
-                    str(tol) + ' > regr.log'
-        print('Running ' + cmd)
+            if platform == 'darwin' or platform == 'linux':
+                cmd = uqtkbin + 'regression -x xdata.dat -y ydata.dat -b PC_MI -s ' + pc_type +       \
+                        ' -p mi.dat -w regparams.dat -m ' + pred_mode + ' -r wbcs -t xcheck.dat -c ' + \
+                        str(tol) + ' > regr.log'
+            elif platform == 'win32':
+                cmd = uqtkbin + 'regression.exe -x xdata.dat -y ydata.dat -b PC_MI -s ' + pc_type +   \
+                        ' -p mi.dat -w regparams.dat -m ' + pred_mode + ' -r wbcs -t xcheck.dat -c ' + \
+                        str(tol) + ' > regr.log'
+            print('Running ' + cmd)
 
-        os.system(cmd)
+            os.system(cmd)
 
-        # Get the PC coefficients and multiindex and the predictive errorbars
-        pccf   = np.loadtxt('coeff.dat')
-        mindex = np.loadtxt('mindex_new.dat')
+            # Get the PC coefficients and multiindex and the predictive errorbars
+            pccf   = np.loadtxt('coeff.dat')
+            mindex = np.loadtxt('mindex_new.dat')
 
-        # Append the results
-        pccf_all.append(pccf)
-        mindex_all.append(mindex)
+            # Append the results
+            pccf_all.append(pccf.tolist())
+            mindex_all.append(mindex.tolist())
 
-        # Evaluate surrogate at training points
-        print('Evaluating surrogate at %d training points' % ntrain)
-        ytrain_pc[:,i] = model_pc(uqtkbin,xtrain,pccf,mindex,pars,del_opt)
-        err_train[i]   = np.linalg.norm(ytrain[:,i]-ytrain_pc[:,i])/np.linalg.norm(ytrain[:,i])
-        print('Surrogate relative error at training points : ' + str(err_train[i]))
+            # Evaluate surrogate at training points
+            print('Evaluating surrogate at %d training points' % ntrain)
+            ytrain_pc[:,i] = model_pc(uqtkbin,xtrain,pccf,mindex,pars,del_opt)
+            err_train[i]   = np.linalg.norm(ytrain[:,i]-ytrain_pc[:,i])/np.linalg.norm(ytrain[:,i])
+            print('Surrogate relative error at training points : ' + str(err_train[i]))
 
-        # Evaluate surrogate at validating points
-        print('Evaluating surrogate at %d validating points' % nval)
-        yval_pc[:,i] = model_pc(uqtkbin,xval,pccf,mindex,pars,del_opt)
-        err_val[i]   = np.linalg.norm(yval[:,i]-yval_pc[:,i])/np.linalg.norm(yval[:,i])
-        print('Surrogate relative error at validating points : ' + str(err_val[i]))
-    
+            # Evaluate surrogate at validating points
+            print('Evaluating surrogate at %d validating points' % nval)
+            yval_pc[:,i] = model_pc(uqtkbin,xval,pccf,mindex,pars,del_opt)
+            err_val[i]   = np.linalg.norm(yval[:,i]-yval_pc[:,i])/np.linalg.norm(yval[:,i])
+            print('Surrogate relative error at validating points : ' + str(err_val[i]))
+        
     if run_in_parallel:
         os.chdir(cur_dir)
 
@@ -221,8 +229,13 @@ def model_inf(uqtkbin, X, Y, pars, mindex_all, pccf_all, del_opt, cur_dir=None, 
 def model_pc(uqtkbin, x, pccf, mindex, pars, del_opt):
     """PC surrogate evaluator"""
 
-    np.savetxt('mindex.dat',mindex,fmt='%d')
-    np.savetxt('pccf.dat',pccf)
+    if pccf.size == 1:
+        np.savetxt('pccf.dat',pccf*np.ones((1,1)))
+        np.savetxt('mindex.dat',np.reshape(mindex,(1,len(mindex))),fmt='%d')
+    else:
+        np.savetxt('pccf.dat',pccf)
+        np.savetxt('mindex.dat',mindex,fmt='%d')
+
     pctype=pars['pc_type']
 
     np.savetxt('xdata.dat',x)
