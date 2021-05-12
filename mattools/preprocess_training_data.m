bbcca8bdde1eafa54ff6419b0a4753de9053deba
donghuix <donghui.xu@pnnl.gov>
2021-05-12 10:44:35 -0700

    Create preprocess_training_data.m

---
+++ mattools/preprocess_training_data.m
@@ -0,0 +1,27 @@
+function [xtrain,ytrain,ntrain,xval,yval,nval] = preprocess_training_data(xtrain,ytrain,xval,yval,threhold)
+    if nargin == 4
+        threhold = 2;
+    end
+    
+    [ntrain,nout] = size(ytrain);
+    [nval,nout] = size(yval);
+    
+    yall = [ytrain; yval];
+    xall = [xtrain; xval];
+    ratio = ntrain / (ntrain+nval);
+    
+    ibad = find(nanmean(yall,2) > threhold * nanmean(nanmean(yall,1)));
+    yall(ibad,:) = [];
+    xall(ibad,:) = [];
+    
+    [ntot,nout] = size(yall);
+    ntrain = ceil(ratio*ntot);
+    nval   = ntot - ntrain;
+    
+    ytrain = yall(1:ntrain,:);
+    yval   = yall(ntrain+1:ntrain+nval,:);
+    xtrain = xall(1:ntrain,:);
+    xval   = xall(ntrain+1:ntrain+nval,:);   
+    
+end
+
