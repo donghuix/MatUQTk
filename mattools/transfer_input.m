1d851f184592f9de014661973b494a3b19893541
donghuix <donghui.xu@pnnl.gov>
2022-03-29 09:50:42 -0700

    add debug for transfer_input.m

--- mattools/transfer_input.m
+++ mattools/transfer_input.m
@@ -1,4 +1,12 @@
-function y = transfer_input(x,lb,ub)
+function y = transfer_input(x,lb,ub,debug)
+    if nargin == 3
+        debug = 0;
+    end
+    if debug
+        disp(['size(x) =' num2str(size(x))]);
+        disp(['size(lb) =' num2str(size(lb))]);
+        disp(['size(ub) =' num2str(size(ub))]);
+    end
     y = x.*(ub - lb)./2 + (ub + lb)./2;
 end
 
